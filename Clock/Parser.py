4551b7bb2590739aa235879f7a869f813130dba0
Rostislav <rostiabrslav@gmail.com>
2020-04-04 02:58:12 +1000

    some commit

---
+++ Clock/Parser.py
@@ -0,0 +1,11 @@
+
+
+dict_of_numbers = dict.fromkeys(['zero', 'one', 'two', 'tree', 'four', 'five', 'six', 'seven', 'eight', 'nine'], [])
+
+with open('etalon', 'r') as file:
+    for line in file:
+        pass
+
+
+
+
