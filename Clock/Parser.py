c1a4dee5a9ed8f800bd764fc19f6dfb25730d799
Rostislav <rostiabrslav@gmail.com>
2020-04-05 16:56:21 +1000

    some commit

--- Clock/Parser.py
+++ Clock/Parser.py
@@ -1,11 +1,19 @@
 
+list_numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+dict_of_numbers = dict.fromkeys(list_numbers, [])
 
-dict_of_numbers = dict.fromkeys(['zero', 'one', 'two', 'tree', 'four', 'five', 'six', 'seven', 'eight', 'nine'], [])
+
+"""что нужно сдедать прочитать файл по строкам и занести в список списокво в словаре
+а для этого нуно итерироваться по файлу построчно и записывать строку в массив в значении словаря 
+при этом ключ должен быть словаря
+нужно как то итерироватьсяпо файлу с промежутком в 8 строк 
+первая строка начинается с нуля значит мы считываем след восемь строк в массив нуля"""
 
 with open('etalon', 'r') as file:
-    for line in file:
-        pass
+    list_file = [line.strip('\n') for line in file]
 
+
+# print(dict_of_numbers)
 
 
 
